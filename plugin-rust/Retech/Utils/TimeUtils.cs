c12fb2ca461a437f6833bb2f33160692f3862088
Kevin van der Burgt <mail@kvdb.dev>
2025-08-20 21:54:30 +0200

    fix: timeutils

--- plugin-rust/Retech/Utils/TimeUtils.cs
+++ plugin-rust/Retech/Utils/TimeUtils.cs
@@ -1,23 +1,12 @@
-using System;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 
 namespace Retech.Utils;
 
 public static class TimeUtils
 {
-  private static readonly long EpochMilliseconds = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
   private static readonly long StartTimestamp = Stopwatch.GetTimestamp();
-  private static readonly double TickFrequency = 1000.0 / Stopwatch.Frequency;
 
-  public static long UnixTimeMilliseconds()
-  {
-    long elapsed = Stopwatch.GetTimestamp() - StartTimestamp;
-    return EpochMilliseconds + (long)(elapsed * TickFrequency);
-  }
+  public static long UnixTimeMilliseconds() => (Stopwatch.GetTimestamp() - StartTimestamp) * 1000L / Stopwatch.Frequency;
 
-  public static long UnixTimeSeconds()
-  {
-    return UnixTimeMilliseconds() / 1000;
-  }
+  public static long UnixTimeSeconds() => UnixTimeMilliseconds() / 1000;
 }
